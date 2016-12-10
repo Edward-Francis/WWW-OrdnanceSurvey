f24cc6204c3d6d44dafb24dcd2c6259525c80995
Edward Francis <edwardafrancis@hotmail.com>
2016-12-10 15:35:26 +0000

    added api definition openplaces

--- lib/WWW/OrdnanceSurvey/API.pm
+++ lib/WWW/OrdnanceSurvey/API.pm
@@ -8,11 +8,89 @@ my %API = (
     opennames => {
         find => {
             method => 'GET',
-            qs     => [qw( query format maxresults offset bounds fq )],
+            qs =>
+                [ 'query', 'format', 'maxresults', 'offset', 'bounds', 'fq' ],
         },
         nearest => {
             method => 'GET',
-            qs     => [qw( point radius format )],
+            qs     => [ 'point', 'radius', 'format' ],
+        },
+    },
+
+    # documentation: https://apidocs.os.uk/docs/os-places-overview
+    openplaces => {
+
+        match => {
+            method => 'GET',
+            qs     => [
+                'query',      'format',
+                'maxresults', 'offset',
+                'dataset',    'lr',
+                'minmatch',   'matchprecision',
+                'fq',         'output_srs'
+            ],
+        },
+
+        find => {
+            method => 'GET',
+            qs     => [
+                'query',      'format',
+                'maxresults', 'offset',
+                'dataset',    'lr',
+                'minmatch',   'matchprecision',
+                'fq',         'output_srs'
+            ],
+        },
+
+        postcode => {
+            method => 'GET',
+            qs     => [
+                'postcode', 'format', 'maxresults', 'offset',
+                'dataset',  'lr',     'fq',         'output_srs'
+            ],
+        },
+
+        uprn => {
+            method => 'GET',
+            qs => [ 'uprn', 'format', 'dataset', 'lr', 'fq', 'output_srs' ],
+        },
+
+        nearest => {
+            method => 'GET',
+            qs     => [
+                'point', 'radius', 'format',     'dataset',
+                'lr',    'fq',     'output_srs', 'srs',
+            ],
+        },
+
+        bbox => {
+            method => 'GET',
+            qs     => [
+                'bbox',    'format', 'maxresults', 'offset',
+                'dataset', 'lr',     'fq',         'output_srs',
+                'srs'
+            ],
+        },
+
+        radius => {
+            method => 'GET',
+            qs     => [
+                'point',      'radius',  'format', 'maxresults',
+                'offset',     'dataset', 'lr',     'fq',
+                'output_srs', 'srs'
+            ],
+
+        },
+
+        polygon => {
+            method => 'POST',
+            qs     => [
+                'referencepoint', 'maxresults',
+                'dataset',        'offset',
+                'lr',             'fq',
+                'output_srs',     'srs'
+            ],
+            body => [ 'type', 'geometry' ],
         },
     },
 
